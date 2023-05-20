e9892fa2cafd0225f632a30684c1d44f65856a9b
Smith <111470120+Smith2067@users.noreply.github.com>
2023-05-19 18:09:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function submitEmail(){
+    event.preventDefault();
+    //I don't actually know what to do here.
+    console.log("I don't actually know what to do here.");
+}
+
+document.getElementById("email-submit-button").addEventListener("click", submitEmail);
